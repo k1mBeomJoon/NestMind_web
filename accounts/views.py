6572ed34d778f8b220c9c43124c5d7b7da6a7d3b
k1mbeomjoon <k1mbeomjoon@example.com>
2025-07-18 21:38:04 +0900

    feat : 메인 페이지, Header, Footer 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from accounts.forms import SignupForm
+from django.contrib.auth import views as auth_views
+from django.http import HttpRequest
 
 def signup(request):
     if request.method == "POST":
@@ -12,3 +14,8 @@ def signup(request):
     else:
         form = SignupForm()
     return render(request, 'accounts/signup.html', {'form': form})
+
+def login_choice(request: HttpRequest):
+    if request.GET.get('real') == '1':
+        return auth_views.LoginView.as_view(template_name='accounts/login_real.html')(request)
+    return render(request, 'accounts/login.html')
